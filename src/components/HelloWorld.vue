f110299d26c81a14f8a32408dda669d5cab77046
suminiee <suminn0816@naver.com>
2024-10-14 15:24:36 +0900

    Update final-project with changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,121 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="form-wrapper">
+
+      
+      <div class="form">
+        <hr>
+        <div class="input-field">
+          <label for="brand">키워드</label>
+          <input type="text" id="brand" v-model="brand" placeholder="키워드 입력">
+        </div>
+        <div class="input-field">
+          <label for="description">설명</label>
+          <textarea id="description" v-model="description" placeholder="설명을 입력하세요"></textarea>
+        </div>
+        <button @click="generateRecommendation">추천 내용 생성하기</button>
+      </div>
+
+      
+      <div class="form">
+        <hr>
+        <div class="input-field">
+          <label for="brand2">제목</label>
+          <input type="text" id="brand2" v-model="brand2" placeholder="제목 입력">
+        </div>
+        <div class="input-field">
+          <label for="description2">내용</label>
+          <textarea id="description2" v-model="description2" placeholder="내용을 입력하세요"></textarea>
+        </div>
+      </div>
+
+      
+      <div class="form">
+        <hr>
+        <div class="input-field">
+          <label for="preview">미리보기</label>
+          <!-- 두 번째 블록의 description2를 placeholder로 설정 -->
+          <textarea id="preview" v-model="preview" :placeholder="description2"></textarea>
+        </div>
+        <button @click="save">저장</button>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      brand: '',
+      description: '',
+      brand2: '',
+      description2: '',
+      preview: ''
+    };
+  },
+  methods: {
+    generateRecommendation() {
+      console.log('추천 내용 생성:', this.brand, this.description);
+    },
+    save() {
+      console.log('저장:', this.preview);
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40vh; 
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.form-wrapper {
+  display: flex; 
+  justify-content: space-between;
+  width: 90%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.form {
+  width: 30%; 
+  padding: 20px;
+  border-radius: 10px;
 }
-a {
-  color: #42b983;
+
+.input-field {
+  margin-bottom: 15px;
+}
+
+input, textarea {
+  width: 100%; 
+  padding: 10px;
+  margin-top: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-family: inherit;
+  font-size: inherit;
+}
+
+textarea {
+  height: 100px; 
+}
+
+button {
+  width: 100%; 
+  padding: 10px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #36996e;
 }
 </style>
